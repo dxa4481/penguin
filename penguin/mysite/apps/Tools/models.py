b9af39d7a5ba0035ee0e3386ee6dc19fed7d9273
dki9992 <dki9992@adf32048-93eb-11e3-923a-07678a7a3859>
2014-03-02 21:59:52 +0000

    -implemented static methods from Tool, User, and OwnTool in Tools/models.py and Users/models.py
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@26 adf32048-93eb-11e3-923a-07678a7a3859

--- penguin/mysite/apps/Tools/models.py
+++ penguin/mysite/apps/Tools/models.py
@@ -3,95 +3,131 @@ from django.db import models
 from ..Users.models import User
 
 class Tool(models.Model):
-        id = models.AutoField(primary_key=True)
-        name = models.CharField(max_length=30)
-        #owner = models.ForeignKey('User')
-        is_available = models.BooleanField(default=True)
-        description = models.CharField(max_length=250)
-        tool_type = models.CharField(max_length=30)
+	id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=30)
+	#owner = models.ForeignKey('User')
+	is_available = models.BooleanField(default=True)
+	description = models.CharField(max_length=250)
+	tool_type = models.CharField(max_length=30)
+	
+	def __str__(self):
+		return (str(self.id) + ':' + self.name)
 
-        """ Constructor to add a new tool
-        :param n: name of tool
-        :param d: description of tool
-        :param t: type of tool
-        :return The tool that was just added
-        """
-        def add_new_tool(self, n, d, tt):
-                t = Tool(name=n, description=d, tool_type=tt)
-                t.save()
-                return t
+	""" Constructor to add a new tool
+	STATIC METHOD
+    :param n: name of tool
+    :param d: description of tool
+    :param t: type of tool
+    :return The tool that was just added
+    """
+	@staticmethod
+	def create_new_tool(n, d, tt):
+		t = Tool(name=n, description=d, tool_type=tt)
+		t.save()
+		return t
+		
+	""" Deletes the given tool
+	STATIC METHOD
+	:param toolID: tool's ID
+	"""
+	@staticmethod
+	def delete_tool(toolID):
+		t = Tool.get_tool(toolID)
+		t.delete()
 
-        """ Deletes the given tool
-        """
-        def delete_tool(self):
-                self.delete()
+	""" Returns tool based on ID
+	"""
+	@staticmethod
+	def get_tool(toolID):
+		return Tool.objects.get(pk=toolID)
+		
+		
+	""" Return tool's ID
+	"""
+	def get_tool_id(self):
+		return self.id
 
-        """Sets a tool as unavailable
-        """
-        def set_tool_unavailable(self):
-                self.is_available = False
-                self.save()
+	"""Sets a tool as unavailable
+	STATIC METHOD
+	:param toolID: tool's ID
+	"""
+	@staticmethod
+	def set_tool_unavailable(toolID):
+		t = Tool.get_tool(toolID)
+		t.is_available = False
+		t.save()
 
-        """ Sets a tool as available
-        """
-        def set_tool_available(self):
-                self.is_available = True
-                self.save()
+	""" Sets a tool as available
+	STATIC METHOD
+	:param toolID: tool's ID
+	"""
+	@staticmethod
+	def set_tool_available(toolID):
+		t = Tool.get_tool(toolID)
+		t.is_available = True
+		t.save()
 
-        """Checks if tool is available
-        :return true if available, false otherwise
-        """
-        def is_tool_available(self):
-                if (self.is_available==True):
-                        return True
-                else:
-                        return False
+	"""Checks if tool is available
+	STATIC METHOD
+	:param toolID: tool's ID
+	:return true if available, false otherwise
+	"""
+	@staticmethod
+	def is_tool_available(toolID):
+		t = Tool.get_tool(toolID)
+		return t.is_available
 
-        """ Get tool's owner's id
-        :return owner's id
-        """
-        def get_tool_owner(self):
-                return OwnTool.filter(tool=self.id).owner
-
-        """ Return tool's ID
-        """
-        def get_tool_id(self):
-                return self.id
-
-        """ Returns tool based on ID
-        """
-        def get_tool(toolID):
-                return Tool.objects.filter(pk=toolID)
+	#INCOMPLETE
+	""" Get tool's owner's id
+	STATIC METHOD
+	:param toolID: tool's ID
+	:return owner's id
+	"""
+	@staticmethod
+	def get_tool_owner(toolID):
+		return OwnTool.get(tool=toolID).owner
 
 
 class OwnTool(models.Model):
-        id = models.AutoField(primary_key=True)
-        owner = models.ForeignKey(User)
-        tool = models.ForeignKey(Tool)
+	id = models.AutoField(primary_key=True)
+	owner = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+	tool = models.ForeignKey(Tool)
 
-        """Create a new tool ownership
-        :param o: owner object
-        :param t: tool object
-        """
-        def add_new_tool_ownership(o, t):
-                own = OwnTool(owner=User.get_user(o),
-                tool=Tool.get_tool(t))
-                own.save()
+	def __str__(self):
+		return (str(self.id) + ' o:' + str(self.owner) + ' t:' + str(self.tool))
+	
+	
+	"""Create a new tool ownership
+	STATIC METHOD
+	:param o: owner object
+	:param t: tool object
+	"""
+	@staticmethod
+	def create_new_tool_ownership(o, t):
+		own = OwnTool(owner=o,
+		tool=t)
+		own.save()
 
-        """ Remove a tool ownership
-        """
-        def remove_tool_ownership(self):
-                self.delete()
+	""" Remove a tool ownership
+	STATIC METHOD
+	:param otID: OwnTool relation's ID
+	"""
+	@staticmethod
+	def remove_tool_ownership(otID):
+		ot = OwnTool.get_tool_ownership(otID)
+		ot.delete()
 
-        """ Return a tool ownership by id
-        """
-        #Not implemented. What should this return?      
-        def get_tool_ownership(id):
-                return null
+	""" Return a tool ownership by id
+	STATIC METHOD
+	:param otID: OwnTool relation's ID
+	"""
+	@staticmethod
+	def get_tool_ownership(otID):
+		return OwnTool.objects.get(pk=otID)
 
-        """Get a tool ownership's id
-        :return id of tool ownership
-        """
-        def get_tool_ownership_id(self):
-                return self.id
+	"""Get a tool ownership's id
+	:return id of tool ownership
+	"""
+	def get_tool_ownership_id(self):
+		return self.id
 
--- penguin/mysite/apps/Users/models.py
+++ penguin/mysite/apps/Users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 """ User object
 """
 class User(models.Model):
@@ -16,8 +17,134 @@ class User(models.Model):
 	def __str__(self):
 		return (str(self.id) + ':' + self.username)
 
+		
+	""" Constructor for a user entry
+	STATIC METHOD
+	:param u: username string
+	:param p: password string
+	:param ac: area code string
+	:param e: email string (forced email field type)
+	:param pn: phone number string
+	"""
+	@staticmethod
+	def create_new_user(u, p, ac, e, pn):
+		newuser = User(username=u, password=p,
+		area_code=ac, email=e, 
+		phone_number=pn)
+		newuser.save()
+		
+	""" Constructor for a community shed entry
+	STATIC METHOD
+	:param ac: area code, used as username as well
+	"""
+	@staticmethod
+	def create_new_community_shed(ac):
+		cs = User(username=ac, password="", 
+		area_code=ac, email="", phone_number="",
+		is_community_shed=True)
+		cs.save()
+		
+		
+	""" Returns a user based on user's ID
+	STATIC METHOD
+	:param userID: user's ID
+	:return User object
+	"""
+	@staticmethod
+	def get_user(userID):
+		return User.objects.get(pk=userID)
+		
+	""" Promotes user object to admin status
+	STATIC METHOD
+	:param userID: user's ID
+	"""
+	@staticmethod
+	def promote_user_to_admin(userID):
+		u = User.get_user(userID)
+		u.is_admin = True;
+		u.save()
+	
+	""" Demotes user object from admin status
+	STATIC METHOD
+	:param userID: user's ID
+	"""
+	@staticmethod
+	def demote_user_from_admin(userID):
+		u = User.get_user(userID)
+		u.is_admin = false;
+		u.save()
+	
+	""" Promotes user object to shed coordinator
+	STATIC METHOD
+	:param userID: user's ID
+	"""
+	@staticmethod
+	def promote_user_to_shed_coordinator(userID):
+		u = User.get_user(userID)
+		u.is_shed_coordinator = True;
+		u.save()
+	
+	""" Demotes user object from shed coordinator
+	STATIC METHOD
+	:param userID: user's ID
+	"""
+	@staticmethod
+	def demote_user_from_shed_coordinator(userID):
+		u = User.get_user(userID)
+		u.is_shed_coordinator = False;
+		u.save()
+		
+	""" Checks if a user is an admin
+	STATIC METHOD
+	:param userID: user's ID
+	"""
+	@staticmethod
+	def is_user_admin(userID):
+		u = User.get_user(userID)
+		return u.is_admin
+		
+	""" Checks if a user is a shed coordinator
+	STATIC METHOD
+	:param userID: user's ID
+	"""
+	@staticmethod
+	def is_user_admin(userID):
+		u = User.get_user(userID)
+		return u.is_shed_coordinator
+	
 	""" Get a user ID
 	:return user's ID
 	"""
 	def get_user_id(self):
 		return self.id
+		
+	#INCOMPLETE
+	""" Deletes a user
+	:param userID: user's ID
+	"""
+	@staticmethod
+	def delete_user(userID):
+		u = User.get_user(userID)
+		#delete all tools relating to user
+		u.delete()
+		
+	#INCOMPLETE
+	"""Add a new tool to tools, then relate it to this user
+	:param userID: user's ID
+	:param n: name of tool
+	:param d: description of tool
+	:param t: type of tool
+	"""
+	@staticmethod
+	def create_new_tool(userID, n, d, tt):
+		u = User.get_user(userID)
+		t = Tool.create_new_tool(n, d, tt)
+		OwnTool.create_new_tool_ownership(u, t)
+		
+	#INCOMPLETE
+	""" Returns an array(?) of all user's tools
+	"""
+	def get_all_user_tools(self):
+		ot = OwnTool.objects.filter(owner=self)
+		tools = ot.tool_set
+		return tools
