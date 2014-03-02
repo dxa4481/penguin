51b99aa16062eb8f5998d7a727ce06a6e7978dd1
dxa4481 <dxa4481@adf32048-93eb-11e3-923a-07678a7a3859>
2014-03-02 20:40:07 +0000

    moving views for user into user app
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@25 adf32048-93eb-11e3-923a-07678a7a3859

--- penguin/mysite/apps/Tools/views.py
+++ penguin/mysite/apps/Tools/views.py
@@ -1,3 +1,8 @@
+from django.template.loader import get_template
+from django.template import Context
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
 
 # Create your views here.
--- penguin/mysite/apps/Users/views.py
+++ penguin/mysite/apps/Users/views.py
@@ -1,3 +1,49 @@
+from django.template.loader import get_template
+from django.template import Context
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
+from models import User
+
+
+def login(request):
+        if 'username' in request.session:
+                return HttpResponseRedirect('/user')
+        if request.method == 'POST':
+                form = Login(request.POST)
+                if form.is_valid():
+                        userExists, user = findUser(request.POST['username'])
+                        if userExists:
+                                if user.password == request.POST['password']:
+                                        request.session['username'] = user.username
+                                        return HttpResponseRedirect("/loggedIn")
+                return HttpResponse("Invalid username/password")
+
+
+        form = Login()
+        html = render(request, 'login.html', {'form':form})
+        return HttpResponse(html)
+
+
+
+
+def register(request):
+        form = UserEditor()
+        if request.method == 'POST':
+                form = UserEditor(request.POST)
+                if form.is_valid():
+                        userExists, user = findUser(request.POST['username'])
+                        if userExists:
+                                if user.password != request.POST['password']:
+                                        return HttpResponse("This is an existing username and it has a different password than the one you entered!")
+                                user.areaCode = request.POST['areaCode']
+                        else:
+                                user = User(username=request.POST['username'], password=request.POST['password'], areaCode=request.POST['areaCode'])
+                        user.save()
+                        request.session['username'] = user.username
+                        return HttpResponseRedirect('/loggedIn')
+        html = render(request, 'userEditor.html', {"action":"Register!", "form":form})
+        return HttpResponse(html)
 
 # Create your views here.
