1a5cda0af74cccaaa536e0a3fad35875a2d16555
njl1050 <njl1050@adf32048-93eb-11e3-923a-07678a7a3859>
2014-03-05 18:55:46 +0000

    Updated the user tools display to display all tools from the logged in user.
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@44 adf32048-93eb-11e3-923a-07678a7a3859

--- penguin/mysite/apps/Tools/views.py
+++ penguin/mysite/apps/Tools/views.py
@@ -4,12 +4,23 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-# from models import Tool
+from .models import *
 
 # Display a list of tools
 def user_tools(request):
 	
-	context = {'tool_list': ['hammer','chisel','toothbrush'] }
+	#context = {'tool_list': ['hammer','chisel','toothbrush'] }
+	if 'username' not in request.session:
+		return HttpResponseRedirect('/')
+		
+	#user_id = request.session['id']
+	username = request.session['username']
+	user_id = User.get_user_by_username(username).id
+	context = { 'tool_list': User.get_all_user_tools(user_id),
+				#'tool_list': Tool.objects.all(),
+				'username': username,
+			  }
+	
 	return render(request, 'user_tools.html', context)
 	#return HttpResponse("This is a test of the User Tools page. Quack.")
 
