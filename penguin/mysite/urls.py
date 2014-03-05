004a333148f44b8a4e666c8624087b370d16c8d2
njl1050 <njl1050@adf32048-93eb-11e3-923a-07678a7a3859>
2014-03-05 14:14:53 +0000

    Fixed: typo in root urls.py file regarding url syntax. Works now. App-specific urls.py files still need updates to point at the appropriate routes though. I've got /Tools working with a dummy message for now.
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@36 adf32048-93eb-11e3-923a-07678a7a3859

--- penguin/mysite/urls.py
+++ penguin/mysite/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-
-
 urlpatterns = patterns('',
-    (r'^$', include('mysite.apps.Users.urls')),
-    (r'^register/$', include('mysite.apps.Users.urls')),
+    url(r'^$', include('mysite.apps.Users.urls')),
+    url(r'^register/$', include('mysite.apps.Users.urls')),
+    url(r'^Tools/', include('mysite.apps.Tools.urls')),
 )
