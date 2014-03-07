7cfe554c4a4326ef5428f0aaf4da40a29384fc91
djb3718 <djb3718@adf32048-93eb-11e3-923a-07678a7a3859>
2014-03-07 14:39:44 +0000

    Created funcionality for displaying all of the tools in an area code
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@51 adf32048-93eb-11e3-923a-07678a7a3859

--- penguin/mysite/apps/Browse/views.py
+++ penguin/mysite/apps/Browse/views.py
@@ -4,5 +4,17 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+from ..Tools.models import User, Tool
 
 # Create your views here.
+
+def browse_tools(request):
+	if 'username' not in request.session:
+		return HttpResponseRedirect('/')
+	username = request.session['username']
+	user_ac = User.get_user_by_username(username).area_code
+	context = { 'tool_list': Tool.get_tool_by_area_code(user_ac),
+		'user_ac': user_ac,
+	}
+	html = render(request, 'browse.html', context)
+	return HttpResponse(html)
