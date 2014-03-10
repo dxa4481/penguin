54272696c894bcaa75d75e4d9ab1df8732a1c06c
njl1050 <njl1050@adf32048-93eb-11e3-923a-07678a7a3859>
2014-03-10 00:33:55 +0000

    Added generic test to demonstrate format.
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@77 adf32048-93eb-11e3-923a-07678a7a3859

--- penguin/mysite/apps/Tools/tests.py
+++ penguin/mysite/apps/Tools/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+""" Test the creation and usage of users and tools. """
+class ToolTestCase(TestCase):
+	def setUp(self):
+		# create a new User to run tests on.
+		User.create_new_user('Barney', 'password', 03545, 'purpledinosaur@pbs.org', '1234567890', 'Use your imagination.')
+
+#	def test_case(self):
+
+	def test_obvious_true(self):
+		self.assertEqual("String", "String")
+		self.assertTrue(3.5 > -1)
+		self.assertIsNone(None)
+
+#	def test_obvious_fail(self):
+#		self.assertTrue(False)
+
