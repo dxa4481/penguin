6d51ab30bc310bcfce0730702d90023f8c7ffca3
njl1050 <njl1050@adf32048-93eb-11e3-923a-07678a7a3859>
2014-03-11 04:59:28 +0000

    Started section for BorrowTransaction unit tests.
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@84 adf32048-93eb-11e3-923a-07678a7a3859

--- penguin/mysite/apps/Tools/tests.py
+++ penguin/mysite/apps/Tools/tests.py
@@ -147,10 +147,19 @@ class ToolTestCase(TestCase):
 		self.assertTrue(localTools.filter(pk=23).exists())
 	
 	
+""" Attempt to test users borrowing tools from other users. """
+class BorrowTransactionTestCase(TestCase):
+		
+	def setUp(self):
+		parrot = User.create_new_user("Parrot", "password", "03545", "polly@python.org", "1234567890", "Pining for the fjords.")
+		User.create_new_user('Penguin', 'password', '03545', 'penguin@python.org', '5555551234', 'It\'s on the telly.')
+		Tool.create_new_tool("Rusty Nail", parrot, "Holding up a stuffed parrot", "nail", "Parrot", "Rip it out of the cage.")
+		Tool.create_new_tool("Warhammer of Zillyhoo", parrot, "Its majesty makes you weep.", "hammer", "03545", "Some time travel required.")
+		
 
-#class BorrowTransactionTestCase(TestCase):
-
-#	def test_create_new_borrow_transaction(self):
+	def test_create_new_borrow_transaction(self):
+		return None;
+			
 #	def test_get_borrow_transaction(self):
 #	def test_get_borrower_borrow_transactions(self):
 #	def test_is_current(self):
