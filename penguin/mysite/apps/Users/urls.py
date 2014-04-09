401189ee559d2a9b85b515e8b6ee664b830f3955
njl1050 <njl1050@adf32048-93eb-11e3-923a-07678a7a3859>
2014-04-09 08:27:58 +0000

    Added get_user_tools route
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@91 adf32048-93eb-11e3-923a-07678a7a3859

--- penguin/mysite/apps/Users/urls.py
+++ penguin/mysite/apps/Users/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from .api_routes import user, login
+from .api_routes import user, login, get_user_tools
 from django.contrib import admin
 admin.autodiscover()
 
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
 
     url(r'^api/user', user),
     url(r'^api/login', login),
+	url(r'^api/get_user_tools', get_user_tools),
     #url(r'^user/edit/$', user_editor),
 
 )
