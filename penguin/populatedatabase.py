385c5e55440abe7601367e310b7e9eed3a236bd3
dki9992 <dki9992@adf32048-93eb-11e3-923a-07678a7a3859>
2014-04-19 16:57:14 +0000

    Added functionality to remove database and repopulate automatically
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@110 adf32048-93eb-11e3-923a-07678a7a3859

--- penguin/populatedatabase.py
+++ penguin/populatedatabase.py
@@ -1,19 +1,43 @@
 from mysite.apps.Users.models import User
 from mysite.apps.Browse.models import BorrowTransaction
 from mysite.apps.Tools.models import Tool
+import os
+import subprocess
 
-User.create_new_user('Dan', 'password', '03545', 'dan@dan.com', '1234567890', "email me!")
-User.create_new_user('Andrew', 'password', '03545', 'andrew@andrew.com', '1234567890', "come to my house")
-User.create_new_user('Schmitty', 'password', '03545', 'schmitty@schmitty.com', '1234567890', "come to my work")
-User.create_new_user('Sam', 'password', '03545', 'sam@sam.com', '1234567890', "knock twice, then once, then twice again")
-User.create_new_user('Nick', 'password', '03545', 'nick@nick.com', '1234567890', "use morse code on my door")
 
-Tool.create_new_tool('Screwdriver 3', '1', 'A green screwdriver', 'screwdriver', False, "email me!")
-Tool.create_new_tool('Screwdriver 4', '4', 'A purple screwdriver', 'screwdriver', False, "email me!")
-Tool.create_new_tool('Drill 1', '3', 'A blue drill', 'drill', False, "email me!")
-Tool.create_new_tool('Drill 2', '1', 'An orange drill', 'drill', False, "knock on my door")
-Tool.create_new_tool('Drill 3', '2', 'A purple drill', 'drill', True, "find me at work")
-b = User.get_user(2)
-t = Tool.get_tool(1)
-BorrowTransaction.create_new_borrow_transaction(b, t)
+def pop_db():
+	User.create_new_user('Dan', 'password', '03545', 'dan@dan.com', '1234567890', "email me!")
+	User.create_new_user('Andrew', 'password', '03545', 'andrew@andrew.com', '1234567890', "come to my house")
+	User.create_new_user('Schmitty', 'password', '03545', 'schmitty@schmitty.com', '1234567890', "come to my work")
+	User.create_new_user('Sam', 'password', '03545', 'sam@sam.com', '1234567890', "knock twice, then once, then twice again")
+	User.create_new_user('Nick', 'password', '03545', 'nick@nick.com', '1234567890', "use morse code on my door")
+	
+	Tool.create_new_tool('Screwdriver 3', '1', 'A green screwdriver', 'screwdriver', False, "email me!")
+	Tool.create_new_tool('Screwdriver 4', '4', 'A purple screwdriver', 'screwdriver', False, "email me!")
+	Tool.create_new_tool('Drill 1', '3', 'A blue drill', 'drill', False, "email me!")
+	Tool.create_new_tool('Drill 2', '1', 'An orange drill', 'drill', False, "knock on my door")
+	Tool.create_new_tool('Drill 3', '2', 'A purple drill', 'drill', True, "find me at work")
+	b = User.get_user(2)
+	t = Tool.get_tool(1)
+	BorrowTransaction.create_new_borrow_transaction(b, t)
+	
 
+def dia():
+	try:
+		print("trying to delete db")
+		os.remove("./db.sqlite3")
+		print("removed db")
+	except:
+		print("error removing")
+	
+	try:
+		print("Trying to sync db")
+		subprocess.call(["python3", "manage.py", "syncdb"]) 
+		print("called syncdb")
+		print("populating")
+		pop_db()
+		print("populated database")
+	except:
+		print("error in syncing/populating")
+		
+pop_db()
