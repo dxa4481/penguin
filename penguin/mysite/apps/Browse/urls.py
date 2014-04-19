d8195f92d3a39f1fa62c194f76a30c4b881d1f60
jrh3828 <jrh3828@adf32048-93eb-11e3-923a-07678a7a3859>
2014-04-19 21:35:36 +0000

    Updated implementation of getToolsBorrowing and getToolsLending.
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@127 adf32048-93eb-11e3-923a-07678a7a3859

--- penguin/mysite/apps/Browse/urls.py
+++ penguin/mysite/apps/Browse/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from .api_routes import borrowTransaction
+from .api_routes import borrowTransaction, getToolsBorrowing, getToolsLending
 from django.contrib import admin
 admin.autodiscover()
 
@@ -7,5 +7,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 		url(r'^api/borrowTransaction/$', borrowTransaction),
+		url(r'^api/borrowTransaction/borrowing/(?P<user_id>\w{0,50})/$', getToolsBorrowing),
+		url(r'^api/borrowTransaction/borrowed/(?P<user_id>\w{0,50})/$', getToolsLending),
 
 )
