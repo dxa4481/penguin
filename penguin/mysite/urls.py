28e13bf2bb0499b66cc1d226ca28a0376ae3a047
dxa4481 <dxa4481@adf32048-93eb-11e3-923a-07678a7a3859>
2014-04-19 20:11:03 +0000

    fixing favicon
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@118 adf32048-93eb-11e3-923a-07678a7a3859

--- penguin/mysite/urls.py
+++ penguin/mysite/urls.py
@@ -2,11 +2,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 admin.autodiscover()
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import RedirectView
+
 
 urlpatterns = patterns('',
 	url(r'^', include('mysite.apps.Users.urls')),
 	url(r'^', include('mysite.apps.Tools.urls')),
 	url(r'^', include('mysite.apps.Browse.urls')),
+	url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),
 	url(r'^$', 'django.contrib.staticfiles.views.serve', kwargs={
 		'path': 'index.html'}),
 )
