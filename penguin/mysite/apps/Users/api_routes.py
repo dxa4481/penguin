3273e4c2acb6e6a92615fa81f1a711d1d30fff9b
djb3718 <djb3718@adf32048-93eb-11e3-923a-07678a7a3859>
2014-04-20 02:12:52 +0000

    Added validation to user api_routes including create_user, update_user, and login.  Added a logout api_route function and added api/logout to urls
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@131 adf32048-93eb-11e3-923a-07678a7a3859

--- penguin/mysite/apps/Users/api_routes.py
+++ penguin/mysite/apps/Users/api_routes.py
@@ -5,7 +5,8 @@ import json
 import datetime
 from .models import User
 from ...json_datetime import dt_to_milliseconds, milliseconds_to_dt
-
+from django.core.validators import validate_email, MaxLengthValidator, MinLengthValidator
+from django.core.exceptions import ValidationError
 
 
 @csrf_exempt
@@ -14,11 +15,8 @@ def user(request):
 
 	"""
 	if request.method == 'GET':
-		print(request.session['user'])
 		u_id = request.session['user']['id']
-		print(u_id)
 		user = User.get_user(u_id)
-		print(user)
 		return_user = { "id": user.id,
 				"username": user.username,
 				"zip_code": user.zip_code,
@@ -27,7 +25,6 @@ def user(request):
 				"default_pickup_arrangements": user.default_pickup_arrangements,
 				"is_shed_coordinator": user.is_shed_coordinator,
 				"is_admin": user.is_admin }
-		print(return_user)
 		return HttpResponse(json.dumps(return_user), content_type="application/json")
 	"""
 		username already exists
@@ -35,9 +32,43 @@ def user(request):
 		invalid email
 		invalid zipcode
 		invalid phone number
+		Missing Fields
 	"""
 	if request.method == 'POST':
 		post_data = json.loads(request.body.decode("utf-8"))
+		
+		# if username already exists -- error 409
+		if( User.get_user_by_username(post_data["username"]) != False ):
+			error = { "error": "Username already exists" }
+			return HttpResponse(json.dumps(error), content_type ="application/json", status=409)
+		# if password doesn't match confirm password -- error 400
+		if( post_data["password"] != post_data["confirm_password"] ):
+			error = {"error": "passwords do not match!"}
+			return HttpResponse(json.dumps(error), content_type="application/json", status=400)
+
+		# if email not a valid email address -- error 400
+		try:
+			validate_email( post_data["email"] )
+		except ValidationError:
+			error = {"error": "You entered an invalid email address!"}
+			return HttpResponse(json.dumps(error), content_type="application/json", status=400)
+
+		# if zipcode not a valid zipcode -- error 400
+		if( len(post_data["zip_code"]) != 5 ):
+			error = {"error": "You entered an invalid zip code!"}
+			return HttpResponse(json.dumps(error), content_type="application/json", status=400)
+		try:
+			is_int = int(post_data["zip_code"])
+		except ValueError:
+			error = {"error": "You entered an invalid zip code!"}
+			return HttpResponse(json.dumps(error), content_type="application/json", status=400)
+
+		# if phone number not a valid phone number -- error 400
+		if( validate_phone_number( post_data["phone_number"] ) == False ):
+			error = {"error": "Invalid phone number, make sure phone number is in form of: 	XXX-XXX-XXXX"}
+			return HttpResponse(json.dumps(error), content_type="application/json", status=400)
+		
+		# else successfull
 		new_user = User.create_new_user(post_data["username"],
 						post_data["password"],
 						post_data["zip_code"],
@@ -53,34 +84,64 @@ def user(request):
 				"is_shed_coordinator": new_user.is_shed_coordinator,
 				"is_admin": new_user.is_admin }
 		request.session['user'] = return_user
-		print(request.session['user'])
 		return HttpResponse(json.dumps(return_user), content_type="application/json")
 	"""
 		password mismatch
 		invalid zipcode
 		invalid email
 		invalid phone number
+		user is shed coorinator can't change zipcode
+		user has tools loaned out, can't change zip
+		user is borrowing tools, can't change zip
 	"""
 	if request.method == 'PUT':
 		put_data = json.loads(request.body.decode("utf-8"))
-		print(put_data)
-		User.update_user(request.session['user']['username'],
-				put_data['phone_number'],
-				put_data['zip_code'],
-				put_data['email'],
-				put_data['default_pickup_arrangements'])
-		user = User.get_user_by_username(request.session['user']["username"])
-		return_user = {	"id": user.id,
-				"username": user.username,
-				"zip_code": user.zip_code,
-				"email": user.email,
-				"phone_number": user.phone_number,
-				"default_pickup_arrangements": user.default_pickup_arrangements,
-				"is_shed_coordinator": user.is_shed_coordinator,
-				"is_admin": user.is_admin }
-		print(return_user)
-		request.session['user'] = return_user
-		print(request.session['user'])
+
+		# invalid zipcode -- error 400
+		if len(put_data["zip_code"]) != 5:
+			error = {"error": "invalid zip code! (wrong len)"}
+			return HttpResponse(json.dumps(error), content_type="application/json", status=400)
+		try:
+			is_int = int(put_data["zip_code"])
+		except ValueError:
+			error = {"error": "invalid zip code! (not an int)"}
+			return HttpResponse(json.dumps(error), content_type="application/json", status=400)
+		
+		# invalid email -- error 400
+		try:
+			validate_email( put_data["email"] )
+		except ValidationError:
+			error = {"error": "You entered an invalid email address!"}
+			return HttpResponse(json.dumps(error), content_type="application/json", status=400)
+
+		# invalid phone number -- error 400
+		if validate_phone_number(put_data["phone_number"]) == False:
+			error = {"error": "invalid phone number, make sure phone number is in form of: XXX-XXX-XXXX!"}
+			return HttpResponse(json.dumps(error), content_type="application/json", status=400)
+
+		# user is shed coordinator, can't change zip -- error 403
+		if request.session['user']['is_shed_coordinator']:
+			error = {"error": "You are currently shed coordinator, you may not change your zip code.  Please contact admin."}
+			return HttpResponse(json.dumps(error), content_type="application/json", status=403)
+
+		# user has tools being borrowed, can't change zip -- error 403
+
+		# user is borrowing tools, can't change zip -- error 403
+
+		update_user = User.update_user( request.session['user']['username'],
+						put_data['phone_number'],
+						put_data['zip_code'],
+						put_data['email'],
+						put_data['default_pickup_arrangements'])
+		return_user = {	"id": update_user.id,
+				"username": update_user.username,
+				"zip_code": update_user.zip_code,
+				"email": update_user.email,
+				"phone_number": update_user.phone_number,
+				"default_pickup_arrangements": update_user.default_pickup_arrangements,
+				"is_shed_coordinator": update_user.is_shed_coordinator,
+				"is_admin": update_user.is_admin} 
+		request.session["user"] = return_user
 		return HttpResponse(json.dumps(return_user), content_type="application/json")
 
 @csrf_exempt
@@ -110,6 +171,17 @@ def login(request):
 	"""
 	if request.method == "POST":
 		post_data = json.loads(request.body.decode("utf-8"))
+		# username does not exist
+		if User.get_user_by_username(post_data["username"]) == False:
+			error = {"error": "Username does not exist"}
+			return HttpResponse(json.dumps(error), content_type="application/json", status=400)
+		
+		# password is incorrect
+		user = User.get_user_by_username(post_data["username"])
+		if post_data["password"] != user.password:
+			error = {"error": "Invalid password"}
+			return HttpResponse(json.dumps(error), content_type="application/json", status=400)
+
 		user = User.get_user_by_username(post_data['username'])
 		return_user = {"id": user.id, 
 				"username" : user.username, 
@@ -120,8 +192,21 @@ def login(request):
 				"is_shed_coordinator": user.is_shed_coordinator, 
 				"is_admin":user.is_admin}
 		request.session['user'] = return_user
-		print(request.session['user'])
 		return HttpResponse(json.dumps(return_user), content_type="application/json")
+
+def validate_phone_number( phone_number ):
+	if( len(phone_number) != 12 ):
+		return False
+	for i in range(12):
+		if i in [3, 7]:
+			if( phone_number[i] != '-' ):
+				return False
+		else:
+			try:
+				is_int = int(phone_number[i])
+			except ValueError:
+				return False
+	return True
 
 @csrf_exempt
 def changePassword(request):
@@ -153,4 +238,12 @@ def changePassword(request):
 		else:
 			return_message = {"error":"incorrect old password"}
 			#return_message = ERROR
+		return HttpResponse(json.dumps(return_message), content_type="application/json")
+
+@csrf_exempt
+def logout(request):
+	if request.method == "DELETE":
+		request.session.flush()
+#		print(request.session["user"])
+		return_message = {"message": "Logout successful!"}
 		return HttpResponse(json.dumps(return_message), content_type="application/json")
--- penguin/mysite/apps/Users/urls.py
+++ penguin/mysite/apps/Users/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from .api_routes import userById, user, login, changePassword
+from .api_routes import userById, user, login, changePassword, logout
 from django.contrib import admin
 admin.autodiscover()
 
@@ -13,6 +13,7 @@ urlpatterns = patterns('',
 	url(r'^api/user$', user),
 	url(r'^api/login$', login),
 	url(r'^api/changePassword', changePassword),
+	url(r'^api/logout', logout),
 	#url(r'^api/get_user_tools', get_user_tools),
 	#url(r'^user/edit/$', user_editor),
 
