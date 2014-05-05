7f6a236c17bdd7ede102664911379fb44b5a19bc
dki9992 <dki9992@adf32048-93eb-11e3-923a-07678a7a3859>
2014-05-05 00:11:39 +0000

    syncdbWithSamples - Added randomized borrow transactions for all states, removed 'drill 1' style tools
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@187 adf32048-93eb-11e3-923a-07678a7a3859

--- penguin/mysite/apps/Users/management/commands/syncdbWithSamples.py
+++ penguin/mysite/apps/Users/management/commands/syncdbWithSamples.py
@@ -20,20 +20,11 @@ class Command(BaseCommand):
 		User.create_new_user('Schmitty', 'password', '03545', 'schmitty@schmitty.com', '1234567890', "come to my work")
 		User.create_new_user('Samantha', 'password', '03545', 'sam@sam.com', '1234567890', "knock twice, then once, then twice again")
 		User.create_new_user('Nick', 'password', '03545', 'nick@nick.com', '1234567890', "use morse code on my door")
-		User.promote_user_to_admin(1)
-
-	
 		
-		Tool.create_new_tool('Drill 1', '1', 'A blue drill', 'drill', False, "email me!")
-		Tool.create_new_tool('Drill 2', '1', 'An orange drill', 'drill', False, "knock on my door")
-		Tool.create_new_tool('Drill 3', '1', 'A purple drill', 'drill', False, "find me at work")
-		Tool.create_new_tool('Drill 4', '1', 'A green drill', 'drill', False, "find me at work")
-		Tool.create_new_tool('Drill 5', '1', 'A yellow drill', 'drill', False, "find me at work")
-		Tool.create_new_tool('Drill 6', '1', 'A black drill', 'drill', False, "find me at work")
-		Tool.create_new_tool('Drill 7', '1', 'A brown drill', 'drill', False, "find me at work")
-		Tool.create_new_tool('Screwdriver 8', '1', 'A green screwdriver', 'screwdriver', True, "email me!")
-		Tool.create_new_tool('Screwdriver 9', '4', 'A purple screwdriver', 'screwdriver', True, "email me!")
-		Tool.create_new_tool('Screwdriver 10', '3', 'A purple screwdriver', 'screwdriver', True, "email me!")
+		#make Dan an admin
+		User.promote_user_to_admin(1)
+		#make Andrew a shed coordinator
+		User.promote_user_to_shed_coordinator(2)
 		
 		avail_date = timezone.now() + datetime.timedelta(days=20)
 		
@@ -53,38 +44,45 @@ class Command(BaseCommand):
 				Tool.create_new_tool('Mr. Wobbles', '4', 'Hits the spot', 'Marital aid', False, 'come inside if you dont see the red pickup truck')
 			Tool.create_new_tool(str(name1 + tool_type), user_num, str("A " + descrip + " " + tool_type), tool_type, in_comm_shed, pickup_arrangement)
 			
-			if (random.choice(range(10)) == 7):
+			bt_choice = random.choice(range(10))
+			if (bt_choice >= 6):
 				t = Tool.get_tool(i)
-				b = User.get_user(user_num)
-				borrow_message = random.choice(['I want this tool please' , 'Can I borrow this?', 'I promise I will give it back soon', 'I really need this', 
-												'You are the best! Please let me use this?'])
-				bt = BorrowTransaction.create_new_borrow_transaction(b, t, borrow_message, avail_date)
-				BorrowTransaction.end_borrow_transaction(bt.id)
+				if (t != False):
+					b = User.get_user(user_num)
+					borrow_message = random.choice(['I want this tool please' , 'Can I borrow this?', 'I promise I will give it back soon', 'I really need this', 
+													'You are the best! Please let me use this?'])
+					bt = BorrowTransaction.create_new_borrow_transaction(b, t, borrow_message, avail_date)
+					if (bt_choice == 6):
+						BorrowTransaction.end_borrow_transaction(bt.id)
+					elif (bt_choice == 7):
+						BorrowTransaction.approve_borrow_transaction(bt.id)
+					elif (bt_choice == 8):
+						BorrowTransaction.request_end_borrow_transaction(bt.id)
 		
 		avail_date = timezone.now() + datetime.timedelta(days=20)
+		dan_tool_list = Tool.get_tool_by_owner(1)
 		#request pending bt ID1
 		b = User.get_user(2)
-		t = Tool.get_tool(1)
+		t = Tool.get_tool(dan_tool_list[0].id)
 		bt = BorrowTransaction.create_new_borrow_transaction(b, t, "Can I haz this tool?", avail_date)
 		#borrowing bt ID2
-		t = Tool.get_tool(2)
+		t = Tool.get_tool(dan_tool_list[1].id)
 		bt = BorrowTransaction.create_new_borrow_transaction(b, t, "I want tool.", avail_date)
 		BorrowTransaction.approve_borrow_transaction(bt.id)
 		#rejected bt ID3
-		t = Tool.get_tool(3)
+		t = Tool.get_tool(dan_tool_list[2].id)
 		bt = BorrowTransaction.create_new_borrow_transaction(b, t, "I wanta da tool.", avail_date)
 		BorrowTransaction.reject_borrow_transaction(bt.id, "screw you")
 		#return pending ID4
-		t = Tool.get_tool(4)
+		t = Tool.get_tool(dan_tool_list[3].id)
 		bt = BorrowTransaction.create_new_borrow_transaction(b, t, "All your tool are belong to us.", avail_date)
 		BorrowTransaction.request_end_borrow_transaction(bt.id)
 		#returned transaction ID5
-		t = Tool.get_tool(5)
+		t = Tool.get_tool(dan_tool_list[4].id)
 		bt = BorrowTransaction.create_new_borrow_transaction(b, t, "All your tool are belong to us.", avail_date)
 		BorrowTransaction.end_borrow_transaction(bt.id)
 		
-		#make Andrew a shed coordinator
-		User.promote_user_to_shed_coordinator(2)
+
 		
 		print("Added samples successfully!")	
 
